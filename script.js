c96900b956970298fac65662be4bf0ad6e72fc9d
θεοχαρης παρισης <parisisxaris@gmail.com>
2025-10-20 17:46:51 +0300

    2nd commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("running");
+
+const taskInput = document.getElementById("taskInput");
+const addTaskButton = document.getElementById("addTaskButton");
+const taskList = document.getElementById("taskList");
+const listDownload = document.getElementById("listDownload");
